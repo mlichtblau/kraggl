ec6f804527d806b3c55ff5f8f892eb5b8a5f560c
Marius Lichtblau <marius@lichtblau.io>
2019-03-27 13:34:03 +0100

    🚧 Save user on first login

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ var express = require('express');
 var router = express.Router();
 var GloBoardApi = require('glo-board-api-node');
 
+const models = require('../models');
+const User = models.User;
+
 const gloBoardApi = new GloBoardApi({ clientId: CLIENT_ID, clientSecret: CLIENT_SECRET });
 
 /* home page. */
@@ -30,15 +33,30 @@ router.get('/callback/oauth', function (req, res, next) {
     .then(data => {
       const accessToken = data.body['access_token'];
       gloBoardApi.setAccessToken(accessToken);
-      return gloBoardApi.getUser();
-    })
-    .then(data => {
-      const user = data.body;
-      console.log(user);
-      req.login(user, function (err) {
-        if (err) { return next(err); }
-        return res.redirect('/me');
-      });
+      gloBoardApi.getUser({ fields: ['id', 'name', 'username'] })
+        .then(data => {
+          const user = data.body;
+          User.findByPk(user.id)
+            .then(existingUser => {
+              if (!existingUser) {
+                return User.create({
+                  id: user.id,
+                  name: user.name,
+                  username: user.username,
+                  gitKrakenAccessToken: accessToken
+                });
+              } else {
+                return existingUser;
+              }
+            })
+            .then(newUser => {
+              req.login(newUser, function (err) {
+                if (err) { return next(err); }
+                return res.redirect('/me');
+              });
+            })
+            .catch(err => { return next(err) });
+        })
     })
     .catch(error => {
       // TODO: Handle error
