0634a80165fdecd064219ff292c1364f0cccf697
bisbaldi <annika.baldi@googlemail.com>
2019-03-29 13:54:21 +0100

    🎨 Work on Modal Functionality

--- public/javascripts/switch.js
+++ public/javascripts/switch.js
@@ -1,11 +1,37 @@
-var button = document.getElementById('customSwitch1');
+$('#settings').click(function() {
+  if ($('#switchTracking').is(':checked')) {
+    $("#onlyonactive").show();
+  }
+  else{
+    $("#onlyonactive").hide();
+  }
+});
 
-button.onclick = function() {
-    var div = document.getElementById('onlyonactive');
-    if (div.style.display !== 'none') {
-        div.style.display = 'none';
-    }
-    else {
-        div.style.display = 'block';
-    }
-};
+$('#switchTracking').click(function() {
+  if ($('#switchTracking').is(':checked')) {
+    $("#onlyonactive").show();
+  }
+  else{
+    $("#onlyonactive").hide();
+  }
+});
+
+$('#saveModalChanges').click(function() {
+  $('input[name="columns[]"]:checked').each(function() {
+     console.log(this.value);
+  });
+});
+
+$(function(){
+  $(".dropdown-menu a.dropdown-item ").click(function(){
+
+      $("#dropdownMenuButton:first-child").text($(this).text());
+      $("#dropdownMenuButton:first-child").val($(this).text());
+
+   });
+
+});
+
+$('#settingsModal').on('hidden.bs.modal', function(){
+    $(this).find('form')[0].reset();
+});
